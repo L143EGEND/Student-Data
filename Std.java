700b99c074d6fec078b647837561fb5a7819b489
Chennakesavulu <134907965+L143EGEND@users.noreply.github.com>
2024-11-06 21:36:39 +0530

    Add files via upload

---
+++ Std.java
@@ -0,0 +1,15 @@
+class Std {
+    public static void main(String args[]) {
+        String Std_name = "KESAV";
+        int Std_id = 1;
+        char Std_gend = 'M';
+        int Std_marks = 75;
+        String Std_subject = "JAVA";
+        
+        System.out.println("Given Student Name: " + Std_name);
+        System.out.println("Given Student ID: " + Std_id);
+        System.out.println("Given Student Gender: " + Std_gend);
+        System.out.println("Given Student Marks: " + Std_marks);
+        System.out.println("Given Student Subject: " + Std_subject);
+    }
+}
